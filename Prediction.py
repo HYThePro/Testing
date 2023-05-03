e031ac48a38494d94b944f930e22dd0980af4046
HYThePro <130675454+HYThePro@users.noreply.github.com>
2023-05-04 03:50:43 +0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -13,11 +13,13 @@ st.title('House Pricing Prediction Web App')
 st.write('This is a testing website for house pricing prediction.\
            Just for testing and practice web deployment for my own use. ')
 
-area = st.sidebar.slider(label = 'house area', min_value = 0, max_value = 1000000,
-                         value = 9999, step = 1)
+area = st.sidebar.slider(label = 'house area', min_value = 0, max_value = 10000,
+                         value = 9000, step = 1)
+bedrooms = st.sidebar.slider(label = 'Number of bedrooms', min_value = 0, max_value = 10,
+                             value = 5, step = 1)
 
 features = {
-  'area': area}
+  'area': area, 'bedrooms':bedrooms}
 
 features_df = pd.DataFrame([features])
 
