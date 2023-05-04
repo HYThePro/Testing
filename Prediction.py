c4b058fe5b03280ae7060c359588bddaa54947ce
HYThePro <130675454+HYThePro@users.noreply.github.com>
2023-05-04 10:34:26 +0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -15,20 +15,38 @@ def normalize(type,val):
         return (val-2.965138)/(0.738064)
   
 st.title('House Pricing Prediction Web App')
-st.write('This is a testing website for house pricing prediction.\
-           Just for testing and practice web deployment for my own use. ')
+st.write("""
+        This is a testing website for house pricing prediction.
+        Just for testing and practice web deployment for my own use.""")
 
-area = st.sidebar.slider(label = 'house area', min_value = 0, max_value = 10000,
-                         value = 9000, step = 1)
-bedrooms = st.sidebar.slider(label = 'Number of bedrooms', min_value = 0, max_value = 10,
-                             value = 5, step = 1)
+st.subheader("Calculation")
+st.write("""
+          Please enter your value after calculated based on the formula below
+          Formula = (X-u)/s
+          X = your value
+          u = mean
+          s = standard deviation""")
+st.text("")
+st.write("---")
+st.text("")
+st.subheader("Mean and standard deviation")
+st.write("""
+         Mean for area = 5150.541284, stadard deviation = 2170.141023
+         Mean for bedrooms = 2.965138, standard deviation = 0.738064""")
+st.text("")
+st.write("---")
+st.text("")
+
+area = st.sidebar.slider(label = 'house area', min_value = -1.56157805, max_value = 5.0308832,
+                         value = 2, step = 0.00000001)
+bedrooms = st.sidebar.slider(label = 'Number of bedrooms', min_value = -2.71770685, max_value = 4.10787026,
+                             value = 3, step = 0.00000001)
 
 features = {
   'area': area, 'bedrooms':bedrooms}
 
-adjusted_features=[normalize("Area",area),normalize("Bedrooms",bedroom)]
 
-input=np.array(adjusted_features).reshape(1,-1)
+input=np.array(features).reshape(1,-1)
 
 features_df  = pd.DataFrame([features])
 st.table(features_df)
