eac567b6b18a392b226148485cc021a499435e0c
HYThePro <130675454+HYThePro@users.noreply.github.com>
2023-05-04 10:37:52 +0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -43,9 +43,9 @@ st.text("")
 st.write("---")
 st.text("")
 
-area = st.sidebar.slider(label = 'house area', min_value = -1.56157805, max_value = 5.0308832,
+area = st.slider(label = 'house area', min_value = -1.56157805, max_value = 5.0308832,
                          value = 2, step = 0.00000001)
-bedrooms = st.sidebar.slider(label = 'Number of bedrooms', min_value = -2.71770685, max_value = 4.10787026,
+bedrooms = st.slider(label = 'Number of bedrooms', min_value = -2.71770685, max_value = 4.10787026,
                              value = 3, step = 0.00000001)
 
 features = {
