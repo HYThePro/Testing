47ac040324f4b221e09d0c24434a378957d28859
HYThePro <130675454+HYThePro@users.noreply.github.com>
2023-05-04 10:35:23 +0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -17,14 +17,19 @@ def normalize(type,val):
 st.title('House Pricing Prediction Web App')
 st.write("""
         This is a testing website for house pricing prediction.
+        
         Just for testing and practice web deployment for my own use.""")
 
 st.subheader("Calculation")
 st.write("""
           Please enter your value after calculated based on the formula below
+          
           Formula = (X-u)/s
+          
           X = your value
+          
           u = mean
+          
           s = standard deviation""")
 st.text("")
 st.write("---")
@@ -32,6 +37,7 @@ st.text("")
 st.subheader("Mean and standard deviation")
 st.write("""
          Mean for area = 5150.541284, stadard deviation = 2170.141023
+         
          Mean for bedrooms = 2.965138, standard deviation = 0.738064""")
 st.text("")
 st.write("---")
