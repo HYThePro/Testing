9172870992cfee5e568ffda793e477f2b9db8370
HYThePro <130675454+HYThePro@users.noreply.github.com>
2023-05-07 01:56:36 +0800

    Update Model.py

--- Model.py
+++ Model.py
@@ -10,7 +10,7 @@ def predict_rating(model, df):
     
     predictions_data = predict_model(estimator = model, data = df)
     
-    return predictions_data['revenue_growthpercent'][9]
+    return predictions_data['revenue_c']
 model = load_model("UMHackathon_model")
 
 def load_model():
