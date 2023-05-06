64dad4df59c8cabdea12134ac638c5c37fb73752
HYThePro <130675454+HYThePro@users.noreply.github.com>
2023-05-07 02:19:45 +0800

    Update Model.py

--- Model.py
+++ Model.py
@@ -60,6 +60,6 @@ input=np.array(adjusted_features).reshape(1, -1)
 
 
 if st.button('Predict'):
-   loaded = load_model()
+   loaded = pd.DataFrame(load_model())
    prediction =loaded.predict(adjusted_features_df)
    st.write('Based on features values, the house price is ' + str(int(prediction)))
